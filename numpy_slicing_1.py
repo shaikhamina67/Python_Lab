71f6f9ed5ff6bffa212878a82bc2d8674e9d29e8
Shaikh Amina <shaikhamina67@gmail.com>
2024-07-22 16:38:34 +0530

    Add files via upload

---
+++ numpy_slicing_1.py
@@ -0,0 +1,22 @@
+#1. Write a NumPy program to create an array of 10 zeros, 10 ones, and 10 fives
+
+import numpy as np
+
+# Create an array of 10 zeros
+zeros = np.zeros(10)
+
+# Create an array of 10 ones
+ones = np.ones(10)
+
+# Create an array of 10 fives
+fives = np.full(10, 5)
+
+# Concatenate the arrays
+result = np.concatenate((zeros, ones, fives))
+
+print(result)
+
+"""Output:
+[0. 0. 0. 0. 0. 0. 0. 0. 0. 0. 1. 1. 1. 1. 1. 1. 1. 1. 1. 1. 5. 5. 5. 5.
+ 5. 5. 5. 5. 5. 5.
+ """
