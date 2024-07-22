fca5d8dd6bf42b287ee27f64c22d1af47cbc61ad
Shaikh Amina <shaikhamina67@gmail.com>
2024-07-22 16:39:12 +0530

    Add files via upload

---
+++ numpy_slicing_2.py
@@ -0,0 +1,17 @@
+#2. Write a NumPy program to create a 3x3 matrix with values ranging from 2 to 10.
+
+import numpy as np
+
+# Create an array with values from 2 to 10
+values = np.arange(2, 11)
+
+# Reshape the array into a 3x3 matrix
+matrix = values.reshape(3, 3)
+
+print(matrix)
+
+"""Output:
+[[ 2  3  4]
+ [ 5  6  7]
+ [ 8  9 10]]
+ """
