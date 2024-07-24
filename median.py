8b9da61af5a67df37c010400766c2b7bbfed72f1
Shaikh Amina <shaikhamina67@gmail.com>
2024-07-24 16:28:38 +0530

    Add files via upload

---
+++ median.py
@@ -0,0 +1,14 @@
+""" 1.Compute the median of the flattened NumPy array 
+
+   Input: x_odd = np.array([1, 2, 3, 4, 5, 6, 7])"""
+
+import numpy as np
+x_odd = np.array([1,2,3,4,5,6,7])
+median_numpy = np.median(x_odd)
+print('median of the flattened Numpy array is',median_numpy)
+
+
+"""
+Output:
+median of the flattened Numpy array is 4.0
+"""
---
+++ standard_deviation.py
@@ -0,0 +1,13 @@
+"""2.Compute the standard deviation of the NumPy array
+
+ Input: arr = [20, 2, 7, 1, 34]"""
+
+import numpy as np
+arr = [20, 2, 7, 1, 34]
+standard_deviation = np.std(arr)
+print('standard_deviation of the numpy array is',standard_deviation)
+
+"""
+Output:
+standard_deviation of the numpy array is 12.576167937809991
+"""
